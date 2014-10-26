d882a667f87c8a3fccd205a1cebe64ebcae32776
Todd A. Shelton <toddsheltontamu@gmail.com>
2014-10-26 14:00:46 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SteelfitProject::Application.config.secret_key_base = '6a431752579a1a59d93dc26df38ca5ac11a20e4a6483fade353aca57696f597f657220a3962e92bacb2aba0e74ec48eb9efa50000af95338665cad1a6709f663'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SteelfitProject::Application.config.session_store :cookie_store, key: '_Steelfit_Project_session'
